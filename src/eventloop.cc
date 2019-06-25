90af398aef584a8926bc9ab8a468dc13854ab91a
root <root@vultr.guest>
2019-06-25 08:14:30 +0000

    完成eventloop设计

--- src/eventloop.cc
+++ src/eventloop.cc
@@ -1,7 +1,65 @@
 #include "eventloop.h"
+#include <sys/epoll.h>
 
 namespace Dandelion{
-    void EventLoop::Add(){};
-    void EventLoop::Del(){};
-    void EventLoop::Run(){};
+    EventLoop::EventLoop()
+    {
+        epoll_fd_ = epoll_create(1024);
+        stop_ = false;
+        time_precise_ = 10;
+        last_active_time_ = 0;
+    }
+
+    int EventLoop::Add(int fd,uint32_t events, FileEvent* file_event){
+        epoll_event e;
+        e.events = events;
+        e.data.ptr = file_event; 
+        return  epoll_ctl(epoll_fd_, EPOLL_CTL_ADD, fd, &e);
+    };
+
+    int EventLoop::Del(int fd){
+        epoll_event e;
+        return  epoll_ctl(epoll_fd_, EPOLL_CTL_DEL, fd, &e);
+    };
+
+    void EventLoop::AddPeriodic(TimedEvent* event){
+        timed_events_.push_back(event);
+    }
+
+    void EventLoop::RemovePeriodic(TimedEvent* event){
+        std::vector<TimedEvent*>::iterator iter;
+        for(iter=timed_events_.begin(); iter!=timed_events_.end(); ++iter){
+            if(*iter == event){
+                timed_events_.erase(iter);
+                break;
+            }
+        }
+    }
+
+    void EventLoop::Run(){
+        while(!stop_){
+            epoll_event events[128];
+            int num = epoll_wait(epoll_fd_, events, 128,time_precise_);
+            if(num<0){
+                spdlog::error("epollwait fail errno={}",errno);
+            }else if(num >0){
+                for(int i=0; i<num; ++i){
+                    epoll_event event = events[i];
+                    ((FileEvent*)event.data.ptr)->callback();
+                }
+            }
+
+            int now = time(NULL);
+            if(now - last_active_time_ >=time_precise_){
+                for(auto timed_event : timed_events_){
+                    timed_event->callback();
+                }
+                last_active_time_ = now;
+            }
+        }
+    };
+
+    void EventLoop::Stop(){
+        stop_ = true;
+    };
 };
--- src/eventloop.h
+++ src/eventloop.h
@@ -1,14 +1,38 @@
 #ifndef DANDELION_EVENTLOOP_H_
 #define DANDELION_EVENTLOOP_H_
 
+#include <stdint.h>
+#include <sys/epoll.h>
+#include <vector>
+#include "spdlog/spdlog.h"
+
 namespace Dandelion{
+
+    class TimedEvent{
+        public:
+        virtual void  callback()=0;
+    };
+    
+    class FileEvent{
+        public:
+        virtual void  callback()=0;
+    };
+
     class EventLoop{
         public:
-            void Add();
-            void Del();
+            EventLoop();
+            int Add(int fd,uint32_t events, FileEvent* file_event);
+            int Del(int fd);
+            void AddPeriodic(TimedEvent* event);
+            void RemovePeriodic(TimedEvent* event);
             void Run();
+            void Stop();
         private:
             int epoll_fd_;
+            std::vector<TimedEvent*> timed_events_;
+            int time_precise_;
+            bool stop_;
+            int last_active_time_;
     };
 };
 #endif
